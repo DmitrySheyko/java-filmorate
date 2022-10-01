0e70a7da181a634e7e6e9d7173a6f3e4f3ffb054
Виктор <Vik2697@yandex.ru>
2022-10-01 13:28:39 +0300

    Добавил класс верью

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+public class Review {
+    private int id;
+    private Integer userId;
+    private Integer filmId;
+    private Boolean isPositive;
+    private String description;
+}
